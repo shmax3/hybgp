846777ec73814951c8d61085642ebb3f3390cb1b
Buzikov Maxim <shmax3@gmail.com>
2018-11-28 02:36:29 +0300

    common interface of evaluator

--- hybgp/evaluators.py
+++ hybgp/evaluators.py
@@ -1,16 +1,20 @@
 import inspect
+import functools
 import numpy as np
 
 
-def evaluator(metric, optimizer, toolbox, args, target):
-
+def evaluator(metrics, kwargs_list, optimizer, toolbox):
     def evaluate(individual):
+        def metric(*args):
+            return sum(functools.partial(m, **k)(*args)
+                       for m, k in zip(metrics, kwargs_list))
+
         func = toolbox.compile(individual)
         args_sign = inspect.signature(func).parameters
         init_appr = np.array([individual.parameters.get(const, .0)
                               for const in args_sign
                               if const.startswith('c')])
-        params, fitness_val, *_ = optimizer(metric, init_appr, (func, args, target))
+        params, fitness_val, *_ = optimizer(metric, init_appr, (func,))
         individual.parameters = dict(zip(args_sign, params))
         return fitness_val,
 
--- hybgp/metrics.py
+++ hybgp/metrics.py
@@ -1,6 +1,15 @@
 import numpy as np
+import inspect
 
 
-def mse(params, func, args, target):
-    val = np.nan_to_num(func(*params, *args))
-    return np.square(val - target).mean()
+def mse(weight):
+    def weight_mse(params, func, args, target):
+        val = np.nan_to_num(func(*params, *args))
+        return weight * np.square(val - target).mean()
+    return weight_mse
+
+
+def parameters_num(weight):
+    def par_num(params, func):
+        return weight * len(inspect.signature(func).parameters)
+    return par_num
