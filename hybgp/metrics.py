aec7ec2c1dd3a1f6a97d8b1eafbcb100c6f5c11f
Buzikov Maxim <shmax3@gmail.com>
2018-12-01 02:45:05 +0300

    sigma2 for mse (chi2)

--- hybgp/metrics.py
+++ hybgp/metrics.py
@@ -2,17 +2,18 @@ import numpy as np
 import inspect
 
 
-def mse(params, func, args, target):
+def mse(params, func, args, target, sigma2=1):
     """Mean squared error. sum((func(*params, *args) - target) ** 2) / N
 
     :param params: A tuple of parameters of *func* function.
     :param func: A function with prototype func(*params, *args).
     :param args: A tuple of the other arguments of *func* function.
     :param target: target - func(*params, *args) must be correct expression.
+    :param sigma2: Dispersions of target points.
     :return: Float value of mean squared error.
     """
     val = np.nan_to_num(func(*params, *args))
-    return np.square(val - target).mean()
+    return (np.square(val - target) / sigma2).mean()
 
 
 def parameters_num(params, func):
